6b1336bfed8a186d76da2eb562aa9006143fd763
dfeldman <dfeldman@dfeldmans-MacBook-Pro.local>
2017-06-06 02:53:02 -0500

    2nd year

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ slides = ""
 with open('input.csv','r') as csvfile:
    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
    for row in reader: 
-      row_d = {'title':row[0], 'speaker':row[1], 'starttime':row[2], 'room':row[3], 'group':row[4], 'img':row[5]}
+      row_d = {'title':row[3], 'speaker':row[5]+" "+row[6], 'starttime':row[9], 'room':row[8], 'group':row[7], 'img':row[2]}
       slides += speaker_slide_template.format(row_d)
 
 print string.replace(deck_template, "{REPLACEWITHSLIDES}", slides + open("ad-slides.html").read())
